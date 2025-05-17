7d36e0c503ee6ac7b4606d342c5624f2d931b967
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2025-05-17 09:46:12 +0100

    added links to more projects

--- src/App/Home.jsx
+++ src/App/Home.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
-
+`
+  a
+  
+`;
 const Home = () => {
   return (
-    <div className="flex flex-wrap w-[80%] gap-4 h-72 mx-auto items-center justify-center">
+    <div className="flex flex-wrap w-[80%] gap-4 h-72 mx-auto mt-16 items-center justify-center">
       <Link to="counter">
         <button className="btn btn-md lg:btn-lg bg-error text-white px-4 text-center">
           Counter App
@@ -18,10 +21,40 @@ const Home = () => {
           Form
         </button>
       </Link>
-      
+
       <a href="https://rock-paper-scissors-psi-ten.vercel.app/">
         <button className="btn btn-md lg:btn-lg bg-orange-400 text-white px-4 text-center">
           RocknPapernScissors
+        </button>
+      </a>
+      <a href="https://day-and-night-brown.vercel.app/">
+        <button className="btn btn-md lg:btn-lg bg-amber-800 text-white px-4 text-center">
+          Day and Night 1
+        </button>
+      </a>
+      <a href="https://spacetourism-seven.vercel.app/">
+        <button className="btn btn-md lg:btn-lg bg-orange-600 text-white px-4 text-center">
+          Space Tourism 
+        </button>
+      </a>
+      <a href="https://feb-cohort-2025-mini-project.vercel.app/">
+        <button className="btn btn-md lg:btn-lg bg-lime-500 text-white px-4 text-center">
+          Day and Night 2
+        </button>
+      </a>
+      <a href="https://feb-cohort-2025-mini-project.vercel.app/horizontalscroll.html">
+        <button className="btn btn-md lg:btn-lg bg-blue-500 text-white px-4 text-center">
+          Horizontalscroll
+        </button>
+      </a>
+      <a href="https://feb-cohort-2025-mini-project.vercel.app/dom-showcase.html">
+        <button className="btn btn-md lg:btn-lg bg-purple-500 text-white px-4 text-center">
+          First Javascript Project
+        </button>
+      </a>
+      <a href="https://feb-cohort-2025-mini-project.vercel.app/animal-notification-ui.html">
+        <button className="btn btn-md lg:btn-lg bg-pink-500 text-white px-4 text-center">
+          Animal notification UI
         </button>
       </a>
     </div>
